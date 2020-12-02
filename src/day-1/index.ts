86b358c18a6e5966345d166b15f02e3a43fb9687
Josh Larson <theMutatedShrimp@gmail.com>
2020-12-01 21:37:38 -0600

    Day 1, Part 2

--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -16,8 +16,30 @@ export function findProductGivenSum(input: string[], sum: number) {
   return x * y;
 }
 
-// const fs = require("fs");
-// const contents = fs.readFileSync("files/day1.txt", "utf8");
+export function findNumbersForSum3(input: string[], sum: number) {
+  for (var i = 0; i < input.length; i++) {
+    const x = parseInt(input[i]);
+    for (var j = 1; j < input.length; j++) {
+      const y = parseInt(input[j]);
+      for (var k = 1; k < input.length; k++) {
+        const z = parseInt(input[k]);
 
-// const input = contents.split("\n");
-// console.log(findProductGivenSum(input, 2020));
+        if (x + y + z === sum) {
+          return [x, y, z];
+        }
+      }
+    }
+  }
+}
+
+export function findProductGivenSum3(input: string[], sum: number) {
+  const [x, y, z] = findNumbersForSum3(input, sum);
+  return x * y * z;
+}
+
+const fs = require("fs");
+const contents = fs.readFileSync("files/day1.txt", "utf8");
+
+const input = contents.split("\n");
+console.log(findProductGivenSum(input, 2020));
+console.log(findProductGivenSum3(input, 2020));

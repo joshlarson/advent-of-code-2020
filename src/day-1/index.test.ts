be8dc4a87ef66dbd3bb095ba64717cb086df82df
Josh Larson <theMutatedShrimp@gmail.com>
2020-12-01 22:28:55 -0600

    Day 1 Refactor

--- src/day-1/index.test.ts
+++ src/day-1/index.test.ts
@@ -1,18 +1,47 @@
 import { findNumbersForSum, findProductGivenSum } from ".";
 
-test("findNumbersForSum with two numbers", () => {
-  expect(findNumbersForSum(["1", "2"], 3)).toStrictEqual([1, 2]);
-  expect(findNumbersForSum(["1", "2"], 5)).toBe(undefined);
+test("findNumbersForSum when the desired list size is 1", () => {
+  expect(findNumbersForSum(["1"], 1, 1)).toStrictEqual([1]);
+  expect(findNumbersForSum(["2"], 1, 1)).toStrictEqual(undefined);
+  expect(findNumbersForSum(["1", "2"], 2, 1)).toStrictEqual([2]);
 });
 
-test("findNumbersForSum with more than two numbers", () => {
-  expect(findNumbersForSum(["1", "2", "4"], 3)).toStrictEqual([1, 2]);
-  expect(findNumbersForSum(["1", "2", "4"], 5)).toStrictEqual([1, 4]);
-  expect(findNumbersForSum(["1", "2", "4"], 6)).toStrictEqual([2, 4]);
+test("findNumbersForSum when the desired list size is 2", () => {
+  expect(findNumbersForSum(["1", "2"], 3, 2)).toStrictEqual([1, 2]);
+  expect(findNumbersForSum(["1", "2"], 5, 2)).toBe(undefined);
+  expect(findNumbersForSum(["1", "2", "4"], 3, 2)).toStrictEqual([1, 2]);
+  expect(findNumbersForSum(["1", "2", "4"], 5, 2)).toStrictEqual([1, 4]);
+  expect(findNumbersForSum(["1", "2", "4"], 6, 2)).toStrictEqual([2, 4]);
 });
 
-test("findProductGivenSum with more than two numbers", () => {
-  expect(findProductGivenSum(["1", "2", "4"], 3)).toBe(2);
-  expect(findProductGivenSum(["1", "2", "4"], 5)).toBe(4);
-  expect(findProductGivenSum(["1", "2", "4"], 6)).toBe(8);
+test("findNumbersForSum when the desired list size is 3", () => {
+  expect(findNumbersForSum(["1", "2", "4"], 7, 3)).toStrictEqual([1, 2, 4]);
+  expect(findNumbersForSum(["1", "2", "4", "8"], 7, 3)).toStrictEqual([
+    1,
+    2,
+    4,
+  ]);
+  expect(findNumbersForSum(["1", "2", "4", "8"], 11, 3)).toStrictEqual([
+    1,
+    2,
+    8,
+  ]);
+  expect(findNumbersForSum(["1", "2", "4", "8"], 13, 3)).toStrictEqual([
+    1,
+    4,
+    8,
+  ]);
+  expect(findNumbersForSum(["1", "2", "4", "8"], 14, 3)).toStrictEqual([
+    2,
+    4,
+    8,
+  ]);
+});
+
+test("findProductGivenSum when the desired list size is 3", () => {
+  expect(findProductGivenSum(["1", "2", "4"], 7, 3)).toStrictEqual(8);
+  expect(findProductGivenSum(["1", "2", "4", "8"], 7, 3)).toStrictEqual(8);
+  expect(findProductGivenSum(["1", "2", "4", "8"], 11, 3)).toStrictEqual(16);
+  expect(findProductGivenSum(["1", "2", "4", "8"], 13, 3)).toStrictEqual(32);
+  expect(findProductGivenSum(["1", "2", "4", "8"], 14, 3)).toStrictEqual(64);
 });
--- src/day-1/index.ts
+++ src/day-1/index.ts
@@ -1,45 +1,47 @@
-export function findNumbersForSum(input: string[], sum: number) {
-  for (var i = 0; i < input.length; i++) {
-    const x = parseInt(input[i]);
-    for (var j = 1; j < input.length; j++) {
-      const y = parseInt(input[j]);
+export function findNumbersForSum(
+  input: string[],
+  sum: number,
+  outputSize: number,
+  startingIndex = 0
+) {
+  if (outputSize === 0) {
+    if (sum == 0) {
+      return [];
+    } else {
+      return undefined;
+    }
+  }
 
-      if (x + y === sum) {
-        return [x, y];
-      }
+  for (var i = 0; i < input.length; i++) {
+    const x = +input[i];
+    const restOfNumbers = findNumbersForSum(
+      input,
+      sum - x,
+      outputSize - 1,
+      startingIndex + 1
+    );
+    if (restOfNumbers != undefined) {
+      return [x].concat(restOfNumbers);
     }
   }
 }
 
-export function findProductGivenSum(input: string[], sum: number) {
-  const [x, y] = findNumbersForSum(input, sum);
-  return x * y;
-}
-
-export function findNumbersForSum3(input: string[], sum: number) {
-  for (var i = 0; i < input.length; i++) {
-    const x = parseInt(input[i]);
-    for (var j = 1; j < input.length; j++) {
-      const y = parseInt(input[j]);
-      for (var k = 1; k < input.length; k++) {
-        const z = parseInt(input[k]);
-
-        if (x + y + z === sum) {
-          return [x, y, z];
-        }
-      }
-    }
+export function findProductGivenSum(
+  input: string[],
+  sum: number,
+  outputSize: number
+) {
+  const numbers: number[] = findNumbersForSum(input, sum, outputSize);
+  var result = 1;
+  for (var x of numbers) {
+    result = result * <number>x;
   }
+  return result;
 }
 
-export function findProductGivenSum3(input: string[], sum: number) {
-  const [x, y, z] = findNumbersForSum3(input, sum);
-  return x * y * z;
-}
+// const fs = require("fs");
+// const contents = fs.readFileSync("files/day1.txt", "utf8");
 
-const fs = require("fs");
-const contents = fs.readFileSync("files/day1.txt", "utf8");
-
-const input = contents.split("\n");
-console.log(findProductGivenSum(input, 2020));
-console.log(findProductGivenSum3(input, 2020));
+// const input = contents.split("\n");
+// console.log(findProductGivenSum(input, 2020, 2));
+// console.log(findProductGivenSum(input, 2020, 3));

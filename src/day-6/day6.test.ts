cece80f99f84eb29b76610f6a9d10e005bc61838
Josh Larson <theMutatedShrimp@gmail.com>
2020-12-06 22:02:21 -0600

    Day 6, Part 2

--- src/day-6/day6.test.ts
+++ src/day-6/day6.test.ts
@@ -1,58 +1,106 @@
 import { Survey, SurveyCollection } from "./day6";
 
 describe("Survey", () => {
-  test("one person, one answer", () => {
-    expect(new Survey(["a"]).answerCount()).toBe(1);
+  describe("answerCount", () => {
+    test("one person, one answer", () => {
+      expect(new Survey(["a"]).answerCount()).toBe(1);
+    });
+    test("one person, two answers", () => {
+      expect(new Survey(["ab"]).answerCount()).toBe(2);
+    });
+    test("two people, two answers", () => {
+      expect(new Survey(["a", "b"]).answerCount()).toBe(2);
+    });
+    test("two people, one answer", () => {
+      expect(new Survey(["a", "a"]).answerCount()).toBe(1);
+    });
+    test("part 1 example", () => {
+      expect(new Survey(["abcx", "abcy", "abcz"]).answerCount()).toBe(6);
+    });
   });
-  test("one person, two answers", () => {
-    expect(new Survey(["ab"]).answerCount()).toBe(2);
-  });
-  test("two people, two answers", () => {
-    expect(new Survey(["a", "b"]).answerCount()).toBe(2);
-  });
-  test("two people, one answer", () => {
-    expect(new Survey(["a", "a"]).answerCount()).toBe(1);
-  });
-  test("part 1 example", () => {
-    expect(new Survey(["abcx", "abcy", "abcz"]).answerCount()).toBe(6);
+
+  describe("unanimousAnswerCount", () => {
+    test("one person, one answer", () => {
+      expect(new Survey(["a"]).unanimousAnswerCount()).toBe(1);
+    });
+    test("one person, two answers", () => {
+      expect(new Survey(["ab"]).unanimousAnswerCount()).toBe(2);
+    });
+    test("two people, two answers", () => {
+      expect(new Survey(["a", "b"]).unanimousAnswerCount()).toBe(0);
+    });
+    test("two people, one answer", () => {
+      expect(new Survey(["a", "a"]).unanimousAnswerCount()).toBe(1);
+    });
+    test("part 1 example", () => {
+      expect(new Survey(["abcx", "abcy", "abcz"]).unanimousAnswerCount()).toBe(
+        3
+      );
+    });
   });
 });
 
 describe("SurveyCollection", () => {
-  test("one family, one person", () => {
-    expect(new SurveyCollection("abc").totalAnswerCount()).toBe(3);
-    expect(new SurveyCollection("abcd").totalAnswerCount()).toBe(4);
+  describe("totalAnswerCount", () => {
+    test("one family, one person", () => {
+      expect(new SurveyCollection("abc").totalAnswerCount()).toBe(3);
+      expect(new SurveyCollection("abcd").totalAnswerCount()).toBe(4);
+    });
+
+    test("one family, two people", () => {
+      expect(new SurveyCollection("abc\n" + "bcd").totalAnswerCount()).toBe(4);
+    });
+
+    test("two families, one person each", () => {
+      expect(
+        new SurveyCollection("abc\n" + "\n" + "bcd").totalAnswerCount()
+      ).toBe(6);
+    });
+
+    test("part 1 example", () => {
+      expect(
+        new SurveyCollection(
+          "abc\n" +
+            "\n" +
+            "a\n" +
+            "b\n" +
+            "c\n" +
+            "\n" +
+            "ab\n" +
+            "ac\n" +
+            "\n" +
+            "a\n" +
+            "a\n" +
+            "a\n" +
+            "a\n" +
+            "\n" +
+            "b"
+        ).totalAnswerCount()
+      ).toBe(11);
+    });
   });
 
-  test("one family, two people", () => {
-    expect(new SurveyCollection("abc\n" + "bcd").totalAnswerCount()).toBe(4);
-  });
-
-  test("two families, one person each", () => {
-    expect(
-      new SurveyCollection("abc\n" + "\n" + "bcd").totalAnswerCount()
-    ).toBe(6);
-  });
-
-  test("part 1 example", () => {
-    expect(
-      new SurveyCollection(
-        "abc\n" +
-          "\n" +
-          "a\n" +
-          "b\n" +
-          "c\n" +
-          "\n" +
-          "ab\n" +
-          "ac\n" +
-          "\n" +
-          "a\n" +
-          "a\n" +
-          "a\n" +
-          "a\n" +
-          "\n" +
-          "b"
-      ).totalAnswerCount()
-    ).toBe(11);
+  describe("totalUnanimousAnswerCount", () => {
+    test("part 1 example", () => {
+      expect(
+        new SurveyCollection(
+          "abc\n" +
+            "\n" +
+            "a\n" +
+            "b\n" +
+            "c\n" +
+            "\n" +
+            "ab\n" +
+            "ac\n" +
+            "\n" +
+            "a\n" +
+            "a\n" +
+            "a\n" +
+            "a\n" +
+            "\n" +
+            "b"
+        ).totalUnanimousAnswerCount()
+      ).toBe(6);
+    });
   });
 });
--- src/day-6/day6.ts
+++ src/day-6/day6.ts
@@ -1,3 +1,9 @@
+function sum(answerCounts: number[]): number {
+  return answerCounts.reduce((a, b) => {
+    return a + b;
+  });
+}
+
 export class Survey {
   answers: string[];
   answerCount(): number {
@@ -8,26 +14,44 @@ export class Survey {
       return set;
     }, new Set()).size;
   }
+  unanimousAnswerCount(): number {
+    return this.answers.reduce((set, str) => {
+      const newSet = new Set();
+      for (var ch of str) {
+        if (set.has(ch)) {
+          newSet.add(ch);
+        }
+      }
+      return newSet;
+    }, new Set(this.answers[0])).size;
+  }
   constructor(answers: string[]) {
     this.answers = answers;
   }
 }
 
 export class SurveyCollection {
-  input: string;
-  totalAnswerCount(): any {
-    const families = this.input.split("\n\n");
-    return families
-      .map((familyInput) => {
-        const answers = familyInput.split("\n");
-        return new Survey(answers).answerCount();
+  surveys: Survey[];
+  totalAnswerCount(): number {
+    return sum(
+      this.surveys.map((survey) => {
+        return survey.answerCount();
       })
-      .reduce((a, b) => {
-        return a + b;
-      });
+    );
+  }
+  totalUnanimousAnswerCount(): number {
+    return sum(
+      this.surveys.map((survey) => {
+        return survey.unanimousAnswerCount();
+      })
+    );
   }
   constructor(input: string) {
-    this.input = input;
+    const families = input.split("\n\n");
+    this.surveys = families.map((familyInput) => {
+      const answers = familyInput.split("\n");
+      return new Survey(answers);
+    });
   }
 }
 
@@ -35,3 +59,4 @@ const fs = require("fs");
 const input = fs.readFileSync("files/day6.txt", "utf8");
 
 console.log(new SurveyCollection(input).totalAnswerCount());
+console.log(new SurveyCollection(input).totalUnanimousAnswerCount());
